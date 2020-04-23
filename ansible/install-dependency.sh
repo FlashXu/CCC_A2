8e8f6f23f203322a97f2744c1353cc1d7f924cb7
Xinglin Qiang <xinglinqiang@gmail.com>
2020-04-23 23:36:51 +0800

    add git pull role

--- ansible/install-dependency.sh
+++ ansible/install-dependency.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-. ./openrc.sh; ansible-playbook -i inventory/hosts.ini install-dependency.yaml
+if [[ $# -gt 0 ]]; then
+    frp=-$1
+fi
+
+. ./openrc.sh; ansible-playbook -i inventory/hosts$frp.ini install-dependency.yaml
