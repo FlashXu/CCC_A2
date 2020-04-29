f0687211ab166a349aaad0974ce69dc5d06fd73c
Yichao Xu <260842931@qq.com>
2020-04-29 16:07:58 +1000

    Web for displaying statistics charts.

---
+++ data_analysis/src/charttype/Linechart.vue
@@ -0,0 +1,19 @@
+<script>
+import { Line } from 'vue-chartjs'
+export default {
+  props:["datapath"],
+  extends: Line,
+  data() {
+    return{
+      linedata: this.datapath.line.data,
+      option:this.datapath.bar.options
+    }
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart(this.linedata, this.option)
+  }
+}
+
+
+</script>
