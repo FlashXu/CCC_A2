827ecf324a447a3b63efbceed8aac573f37af549
Taylor <hwt52026@gmail.com>
2020-05-07 22:23:46 +1000

    map color changed

--- frontend2/src/pages/Maps.vue
+++ frontend2/src/pages/Maps.vue
@@ -7,13 +7,12 @@
 <script>
 import MainFooter from "@/layout/MainFooter";
 import GoogleMapsApiLoader from "google-maps-api-loader";
-
 export default {
   name: "maps-page",
   bodyClass: "maps-page",
-
   methods: {
     initMap: () => {
+      // Styles a map in night mode.
       var AUSSIE_BOUNDS = {
         north: -10,
         south: -44,
@@ -34,7 +33,7 @@ export default {
             elementType: "geometry",
             stylers: [
               {
-                color: "#212121",
+                color: "#f5f5f5",
               },
             ],
           },
@@ -50,7 +49,7 @@ export default {
             elementType: "labels.text.fill",
             stylers: [
               {
-                color: "#757575",
+                color: "#616161",
               },
             ],
           },
@@ -58,41 +57,12 @@ export default {
             elementType: "labels.text.stroke",
             stylers: [
               {
-                color: "#212121",
-              },
-            ],
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#757575",
-              },
-              {
-                visibility: "off",
-              },
-            ],
-          },
-          {
-            featureType: "administrative.country",
-            elementType: "labels.text.fill",
-            stylers: [
-              {
-                color: "#9e9e9e",
+                color: "#f5f5f5",
               },
             ],
           },
           {
             featureType: "administrative.land_parcel",
-            stylers: [
-              {
-                visibility: "off",
-              },
-            ],
-          },
-          {
-            featureType: "administrative.locality",
             elementType: "labels.text.fill",
             stylers: [
               {
@@ -101,27 +71,11 @@ export default {
             ],
           },
           {
-            featureType: "administrative.neighborhood",
-            stylers: [
-              {
-                visibility: "off",
-              },
-            ],
-          },
-          {
             featureType: "poi",
+            elementType: "geometry",
             stylers: [
               {
-                visibility: "off",
-              },
-            ],
-          },
-          {
-            featureType: "poi",
-            elementType: "labels.text",
-            stylers: [
-              {
-                visibility: "off",
+                color: "#eeeeee",
               },
             ],
           },
@@ -139,7 +93,7 @@ export default {
             elementType: "geometry",
             stylers: [
               {
-                color: "#181818",
+                color: "#e5e5e5",
               },
             ],
           },
@@ -148,61 +102,25 @@ export default {
             elementType: "labels.text.fill",
             stylers: [
               {
-                color: "#616161",
-              },
-            ],
-          },
-          {
-            featureType: "poi.park",
-            elementType: "labels.text.stroke",
-            stylers: [
-              {
-                color: "#1b1b1b",
+                color: "#9e9e9e",
               },
             ],
           },
           {
             featureType: "road",
-            elementType: "geometry.fill",
+            elementType: "geometry",
             stylers: [
               {
-                color: "#2c2c2c",
-              },
-            ],
-          },
-          {
-            featureType: "road",
-            elementType: "labels",
-            stylers: [
-              {
-                visibility: "off",
-              },
-            ],
-          },
-          {
-            featureType: "road",
-            elementType: "labels.icon",
-            stylers: [
-              {
-                visibility: "off",
-              },
-            ],
-          },
-          {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [
-              {
-                color: "#8a8a8a",
+                color: "#ffffff",
               },
             ],
           },
           {
             featureType: "road.arterial",
-            elementType: "geometry",
+            elementType: "labels.text.fill",
             stylers: [
               {
-                color: "#373737",
+                color: "#757575",
               },
             ],
           },
@@ -211,21 +129,12 @@ export default {
             elementType: "geometry",
             stylers: [
               {
-                color: "#3c3c3c",
+                color: "#dadada",
               },
             ],
           },
           {
-            featureType: "road.highway.controlled_access",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#4e4e4e",
-              },
-            ],
-          },
-          {
-            featureType: "road.local",
+            featureType: "road.highway",
             elementType: "labels.text.fill",
             stylers: [
               {
@@ -234,19 +143,29 @@ export default {
             ],
           },
           {
-            featureType: "transit",
+            featureType: "road.local",
+            elementType: "labels.text.fill",
             stylers: [
               {
-                visibility: "off",
+                color: "#9e9e9e",
               },
             ],
           },
           {
-            featureType: "transit",
-            elementType: "labels.text.fill",
+            featureType: "transit.line",
+            elementType: "geometry",
             stylers: [
               {
-                color: "#757575",
+                color: "#e5e5e5",
+              },
+            ],
+          },
+          {
+            featureType: "transit.station",
+            elementType: "geometry",
+            stylers: [
+              {
+                color: "#eeeeee",
               },
             ],
           },
@@ -255,16 +174,7 @@ export default {
             elementType: "geometry",
             stylers: [
               {
-                color: "#000000",
-              },
-            ],
-          },
-          {
-            featureType: "water",
-            elementType: "labels.text",
-            stylers: [
-              {
-                visibility: "off",
+                color: "#c9c9c9",
               },
             ],
           },
@@ -273,43 +183,45 @@ export default {
             elementType: "labels.text.fill",
             stylers: [
               {
-                color: "#3d3d3d",
+                color: "#9e9e9e",
               },
             ],
           },
         ],
       });
-
       map.data.loadGeoJson("./GeoJson-Data-master/SA3_2016_AUST_SIM.json", {
         idPropertyName: "isColorful",
       });
       map.data.loadGeoJson("./GeoJson-Data-master/earthquakes.geojson");
-
       map.data.setStyle((feature) => {
         // earthquake circle
         let mag = feature.getProperty("mag");
         var circleMarker = {
           path: google.maps.SymbolPath.CIRCLE,
-          fillColor: "blue",
+          fillColor: "red",
           fillOpacity: 0.4,
           scale: Math.pow(2, mag) / 2,
           strokeColor: "white",
           strokeWeight: 0.5,
         };
-
         // highlight states white when mouseover
         if (feature.getProperty("isColorful")) {
-          var color = "white";
+          var color = "yellow";
           return {
             fillColor: color,
-            icon: circleMarker
+            icon: circleMarker,
+            strokeOpacity: 2,
           };
-        };
-        return {
-          icon: circleMarker
+        } else {
+          // no effect
+          return {
+            icon: circleMarker,
+            //strokeOpacity:0,
+            fillOpacity: 0,
+            strokeWeight: 0.1,
+          };
         }
       });
-
       // auto zoom after click one region
       map.data.addListener("rightclick", (e) => {
         var maker = new google.maps.Marker({
@@ -317,30 +229,23 @@ export default {
           map: map,
         });
       });
-
       map.data.addListener("dblclick", function(e) {
         let bounds = new google.maps.LatLngBounds();
-
         e.feature.getGeometry().forEachLatLng((x) => bounds.extend(x));
         map.fitBounds(bounds);
         map.panToBounds(bounds);
-
       });
-
       map.data.addListener("mouseover", (e) => {
         e.feature.setProperty("isColorful", true);
       });
-
       map.data.addListener("mouseout", (e) => {
         e.feature.setProperty("isColorful", false);
       });
-
       map.addListener("zoom_changed", () => {
         //console.log(map.getZoom());
       });
     },
   },
-
   mounted() {
     const googleMapApi = GoogleMapsApiLoader({
       apiKey: "AIzaSyDXG60896YH8pjO-svO4f7zQlxWBlZHp98",
