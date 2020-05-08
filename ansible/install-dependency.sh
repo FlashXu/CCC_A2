75734bba2a8fbdbeecc89c6a4bebae683a86cbbc
Xinglin Qiang <xinglinqiang@gmail.com>
2020-05-09 04:40:38 +0800

    add backend ansible

--- ansible/install-dependency.sh
+++ ansible/install-dependency.sh
@@ -4,4 +4,4 @@ if [[ $# -gt 0 ]]; then
     frp=-$1
 fi
 
-. ./openrc.sh; ansible-playbook -i inventory/hosts$frp.ini install-dependency.yaml
+ansible-playbook -i inventory/hosts$frp.ini install-dependency.yaml
